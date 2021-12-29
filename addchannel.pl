cd96e6e2979c084f1d74861debf5b3e9643b2f58
Tyhjakuori <88154596+Tyhjakuori@users.noreply.github.com>
2021-12-29 20:40:03 +0200

    Fixed perl script

--- addchannel.pl
+++ addchannel.pl
@@ -12,7 +12,10 @@ chomp $channelname;
 
 print "Id: '$channelid' Name: '$channelname'\n";
 
-my $dir = "/home/username/Videos/$channelname";
+my $username = "";
+#Write here your username
+
+my $dir = "/home/$username/Videos/$channelname";
 mkdir( $dir ) or die "Unable to create the directory:$!";
 open(FH, ">", "$dir/.$channelname") or die "Unable to create dotfile for yt-dlp:$!";
 close FH;
@@ -31,9 +34,9 @@ die "File coudn't be opened";
 \#$channelname
 channelid$number=$channelid
 channelname$number2=$channelname
-cd ~/Videos/\"\$channelname$number2\"/ || {echo "Failure"; exit 1; }
-yt-dlp -f bestvideo+bestaudio --download-archive ~/Videos/\"\$channelname$number2\"/.\"\$channelname$number2\" --merge-output-format mkv https://www.youtube.com/channel/\"\$channelid$number\" || \$(
-curl https://www.youtube.com/feeds/videos.xml?channel_id=\"\$channelid$number\" | grep yt:videoId | sed -e 's/<\\/*yt:videoId>//g' -e 's/ //g' > .ids.txt
+cd ~/Videos/\$channelname$number2\/ || { echo "Failure"; exit 1; }
+yt-dlp -f bestvideo+bestaudio --download-archive ~/Videos/\$channelname$number2\/.\$channelname$number2 --merge-output-format mkv https://www.youtube.com/channel/\$channelid$number || \$(
+curl https://www.youtube.com/feeds/videos.xml?channel_id=\$channelid$number | grep yt:videoId | sed -e 's/<\\/*yt:videoId>//g' -e 's/ //g' > .ids.txt
 find ./* | sed 's/....\$//g' | grep -o '.\\{11\\}\$' > .local.txt
 missing=\$(comm -23 <(sort .ids.txt) <(sort .local.txt))
 for dlvideo in \$missing; do
@@ -43,7 +46,7 @@ for dlvideo in \$missing; do
 	title=\$(yt-dlp -e "\$dlvideo" | sed -e 's/ /_/g' -e 's/\\///g')
 	mv output.mp4 \"\$title\"-\"\$dlvideo\".mp4
 	rm video.mp4 && rm audio.mp4
-	echo youtube \"\$dlvideo\" >> .\"\$channelname$number2\"
+	echo youtube \"\$dlvideo\" >> .\$channelname$number2
 done
 rm .ids.txt && rm .local.txt)
 });
